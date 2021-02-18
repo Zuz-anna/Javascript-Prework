91c580c2d5d9d84a2c0312deed6bb7888865d719
Zuzanna Grzegorzewska <zuzannagrzeg@wp.pl>
2021-02-18 15:32:31 +0100

    Add variables for computer and player moves

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
+console.log('Wylosowana liczba to: ' + randomNumber);
 if (randomNumber == '1') {
   computerMove = 'kamień';
 } else if (randomNumber == '2') {
@@ -9,3 +9,19 @@ if (randomNumber == '1') {
   computerMove = 'nożyce';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce');
+console.log('Wybrana liczba to: ' + playerInput);
+if (playerInput == '1'){
+    playerMove = 'kamień';
+} else if (playerInput == '2'){
+    playerMove = 'papier';
+} else if (playerInput == '3'){
+    playerMove = 'nożyce';
+} 
+printMessage('Twój ruch: ' + playerMove);
+
+else {
+    printMessage('Liczba spoza zakresu');
+}
