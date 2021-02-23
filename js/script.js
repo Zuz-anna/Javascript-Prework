76e1d8c5e3f8619b7c5bee104c560f20472801e4
Zuzanna Grzegorzewska <zuzannagrzeg@wp.pl>
2021-02-23 20:59:12 +0100

    Improve variable declaration and correct use of let and const

--- js/script.js
+++ js/script.js
@@ -1,22 +1,15 @@
-var buttonRock, buttonPaper, buttonScissors, argButtonName, argComputerMove, argMoveId, argPlayerMove, computerMove, playerInput, playerMove, randomNumber, counter1, counter2;
-
-buttonRock = document.getElementById('button-rock');
-buttonPaper = document.getElementById('button-paper');
-buttonScissors = document.getElementById('button-scissors');
-
-counter1 = 0;
-counter2 = 0;
-
+let counter1 = 0, counter2 = 0, buttonRock = document.getElementById('button-rock'), buttonPaper = document.getElementById('button-paper'), buttonScissors = document.getElementById('button-scissors');
 
 function buttonClicked(argButtonName) {
-  clearMessages();
-  console.log(argButtonName + ' został kliknięty');
 
+  clearMessages();
   playerMove = argButtonName;
+
+  console.log(argButtonName + ' został kliknięty');
 
   randomNumber = Math.floor(Math.random() * 3 + 1);
   console.log('wylosowana liczba to: ' + randomNumber);
-
+  
   computerMove = getMoveName(randomNumber);
   console.log('ruch komputera to: ' + computerMove);
 
@@ -25,7 +18,9 @@ function buttonClicked(argButtonName) {
 
 
 function getMoveName(argMoveId) {
+
   console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
+
   if (argMoveId == 1) {
     return 'kamień';
   } else if (argMoveId == 2) {
@@ -40,6 +35,7 @@ function getMoveName(argMoveId) {
 
 
 function displayResult(argPlayerMove, argComputerMove) {
+
   console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
   if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
     printMessage('Wygrywasz!');
@@ -59,7 +55,6 @@ function displayResult(argPlayerMove, argComputerMove) {
   printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
   printResult('Twój wynik to: ' + counter1 + ', wynik komputera to: ' + counter2);
 }
-
 
 buttonRock.addEventListener('click', function(){
   buttonClicked('kamień');});
