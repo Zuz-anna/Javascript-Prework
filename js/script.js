ec204ba7c37b40f5e034bfe20e1bbf51e826a340
Zuzanna Grzegorzewska <zuzannagrzeg@wp.pl>
2021-02-27 12:10:52 +0100

    Add let declaration to randomNumber and computerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let counterPlayerWin = 0;
 let counterComputerWin = 0; 
 const buttonRock = document.getElementById('button-rock');
@@ -12,14 +14,13 @@ function buttonClicked(playerMove) {
   clearMessages();
   
   console.log(playerMove + ' został kliknięty');
-  randomNumber = Math.floor(Math.random() * 3 + 1);
+  let randomNumber = Math.floor(Math.random() * 3 + 1);
   console.log('wylosowana liczba to: ' + randomNumber);
-  computerMove = getMoveName(randomNumber);
+  let computerMove = getMoveName(randomNumber);
   console.log('ruch komputera to: ' + computerMove);
   
   displayResult(playerMove, computerMove);
 }
-
 
 
 
